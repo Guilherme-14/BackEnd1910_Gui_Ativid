49249d47b84aaaaae023340fcf0b36994ed115a2
Guilherme-14 <132360130+Guilherme-14@users.noreply.github.com>
2023-10-19 16:17:53 -0300

    Add files via upload

---
+++ GerencTarefas2/src/main/java/com/projetojpa/GerencTarefas2Application.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerencTarefas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerencTarefas2Application.class, args);
+	}
+
+}
---
+++ GerencTarefas2/src/test/java/com/projetojpa/GerencTarefas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerencTarefas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
